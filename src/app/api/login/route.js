cc98efdab5db8ee2f26896c84de4bf474b90ff77
K-ash-ish <kashishsondhiya@gmail.com>
2023-09-02 21:16:09 +0530

    login page completed

---
+++ src/app/api/login/route.js
@@ -0,0 +1,43 @@
+import { prisma } from "@/lib/db";
+import { compare } from "@/lib/hash";
+import { sign } from "@/lib/jwt";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function POST(req) {
+  const { email, password } = await req.json();
+  const reqUser = await prisma.user.findFirst({
+    where: {
+      email: {
+        equals: email,
+      },
+    },
+  });
+
+  if (reqUser) {
+    const isValidPassword = await compare(reqUser.password, password);
+    if (isValidPassword) {
+      const token = await sign({
+        accessLevel: "user",
+        email: reqUser.email,
+      });
+      const oneDay = 24 * 60 * 60 * 1000;
+
+      cookies().set({
+        name: "token",
+        value: token,
+        httpOnly: true,
+        expires: Date.now() + oneDay,
+      });
+      const userProfile = await prisma.profile.findFirst({
+        where: {
+          userId: {
+            equals: reqUser.id,
+          },
+        },
+      });
+      return NextResponse.json({ message: userProfile }, { status: 200 });
+    }
+  }
+  return NextResponse.json({ error: "wrong credentials" }, { status: 400 });
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,3 +1,4 @@
+"use client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -10,10 +11,39 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Router from "next/router";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import FormFieldComp from "@/components/FormFieldComp";
+
 const formSchema = z.object({
   email: z.string().email(),
   password: z.string(),
 });
+
+function onSubmit(values) {
+  const { email, password } = values;
+  fetch("http://localhost:3000/api/login", {
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+    method: "POST",
+    credentials: "include",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.message === null) {
+        return console.log("Null");
+      }
+      console.log("not null");
+    });
+}
 
 function Login() {
   const form = useForm({
@@ -24,50 +54,24 @@ function Login() {
     },
   });
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input placeholder="email" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input placeholder="password" type="password" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex h-screen items-center">
+      <Card className="w-[350px] mx-auto  ">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to access the app</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormFieldComp form={form} fieldName="email" />
+              <FormFieldComp form={form} fieldName="password" type="password" />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
-function onSubmit(values) {
-  const { email, password } = values;
-  fetch("https://prisma-tut.vercel.app/api/admin/login", {
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-    method: "POST",
-    credentials: "include",
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-}
+
 export default Login;
