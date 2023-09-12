0d4823229bc941ebbdda42119046ddf742fec0d7
K-ash-ish <kashishsondhiya@gmail.com>
2023-09-12 17:13:38 +0530

    individual post page created

---
+++ src/app/api/post/postId/route.js
@@ -0,0 +1,46 @@
+import { prisma } from "@/lib/db";
+import { verify } from "@/lib/jwt";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  const { postId } = await req.json();
+  const token = cookies().get("token")?.value;
+  if (!token) {
+    return NextResponse.json({ message: "Not Authorised" }, { status: 400 });
+  }
+  const isTokenVerified = await verify(token);
+  if (!isTokenVerified) {
+    return NextResponse.json({ message: "Not Authorised" }, { status: 400 });
+  }
+  const post = await prisma.post.findUnique({
+    where: {
+      id: Number(postId),
+    },
+    include: {
+      author: {
+        select: {
+          name: true,
+          userHandle: true,
+        },
+      },
+      comments: {
+        select: {
+          id: true,
+          createdAt: true,
+          updatedAt: true,
+          content: true,
+          author: {
+            select: {
+              name: true,
+              userHandle: true,
+              profilePic: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  console.log(post);
+  return NextResponse.json({ message: "success", post }, { status: 200 });
+}
--- src/app/feed/page.js
+++ src/app/feed/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 function Feed() {
@@ -20,20 +21,21 @@ function Feed() {
     fetchData();
   }, []);
 
-
   return (
     <div className="rounded-md md:w-1/2 md:mx-auto h-[580px]">
       <h1 className="text-3xl font-bold text-center mt-2">Feed</h1>
       {posts?.map((post) => {
         return (
-          <Card key={post.id} className="my-4 mx-2">
-            <CardHeader className="">
-              <CardTitle className="capitalize font-semibold">
-                {post.author.userHandle}
-              </CardTitle>
-              <CardContent className="text-sm">{post.content}</CardContent>
-            </CardHeader>
-          </Card>
+          <Link key={post.id} href={`/post/${post.id}`}>
+            <Card className="my-4 mx-2">
+              <CardHeader className="">
+                <CardTitle className="capitalize font-semibold">
+                  {post.author.userHandle}
+                </CardTitle>
+                <CardContent className="text-sm">{post.content}</CardContent>
+              </CardHeader>
+            </Card>
+          </Link>
         );
       })}
     </div>
