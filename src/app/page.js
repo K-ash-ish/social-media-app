fbac002717684be7a4bfe4f059a1aceea6a0dfce
K-ash-ish <kashishsondhiya@gmail.com>
2023-11-07 15:51:26 +0530

    ui fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,20 @@
 "use client";
 import PostCard from "@/components/PostCard";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useUserPosts } from "@/hooks/useUserPosts";
-import Link from "next/link";
 
 function Feed() {
   const { posts } = useUserPosts();
 
   return (
-    <div className="rounded-md md:w-1/2 md:mx-auto h-5/6 ">
+    <div className=" md:w-1/2 min-h-[83%] md:mx-auto  m-4  ">
       {/* <h1 className="text-3xl font-bold text-center mt-2">Feed</h1> */}
+      {posts?.length === 0 && (
+        <h1 className="text-center">
+          Post from people you follow will be shown here.
+        </h1>
+      )}
+
       {posts?.map((post) => {
         return (
           <PostCard
