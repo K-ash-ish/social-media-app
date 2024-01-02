2382a84237d9db6def3101fb0779f4e1c41f0010
K-ash-ish <kashishsondhiya@gmail.com>
2024-01-02 19:16:43 +0530

    fixing metadata error for client component

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,14 +8,17 @@ import AuthProvider from "./context/AuthContext";
 const roboto = Montserrat({
   subsets: ["latin"],
 });
-export const metadata = {
-  title: "SocialApp",
-  description: "Social media app created using NextJS.",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={roboto.className}>
+      <head>
+        <title>SocialApp</title>
+        <meta
+          name="description"
+          content="Social media app created using NextJS."
+        />
+      </head>
       <body className="h-screen w-full">
         <AuthProvider>
           <Navbar />
